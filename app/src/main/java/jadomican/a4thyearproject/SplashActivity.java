0c2685193474d940f7a718117a31bcbcbbf4d0d4
Jason <jadomican@gmail.com>
2017-10-23 15:43:03 +0100

    Add modules to interact with AWS SDK

---
+++ app/src/main/java/jadomican/a4thyearproject/SplashActivity.java
@@ -0,0 +1,51 @@
+package jadomican.a4thyearproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Context;
+import android.content.Intent;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.auth.core.StartupAuthResultHandler;
+import com.amazonaws.mobile.auth.core.StartupAuthResult;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Context appContext = getApplicationContext();
+        AWSConfiguration awsConfig = new AWSConfiguration(appContext);
+        IdentityManager identityManager = new IdentityManager(appContext, awsConfig);
+        IdentityManager.setDefaultIdentityManager(identityManager);
+        identityManager.doStartupAuth(this, new StartupAuthResultHandler() {
+            @Override
+            public void onComplete(StartupAuthResult startupAuthResult) {
+                // User identity is ready as unauthenticated user or previously signed-in user.
+            }
+        });
+
+        // Go to the main activity
+        final Intent intent = new Intent(this, MainActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        this.startActivity(intent);
+        this.finish();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
